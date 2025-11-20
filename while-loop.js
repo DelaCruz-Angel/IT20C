1cfbc846f6967c72ca66ac462706f127e3425fe7
DelaCruz-Angel <mobilen1cole58@gmail.com>
2025-11-20 08:29:55 +0800

    while

--- while-loop.js
+++ while-loop.js
@@ -1,6 +1,35 @@
-i = 1;
+// i = 1;
 
-while(i<= 5){
-    console.log(i);
-    i++;
+// while(i<= 5){
+//     console.log(i);
+//     i++;
+// }
+
+// console.log("Ascending");
+// x=1;
+
+// while(x<=5){
+//     console.log(x);
+//     x++;
+// }
+
+// output = " ";
+// x=1;
+
+// while(x<=5){
+//     output += x + " ";
+//     x++;
+// }
+
+// console.log(output);
+
+arr=[1,2,3]
+output = " ";
+index= 0;
+
+while(index <=arr.length){
+    output += index + " ";
+    index++;
 }
+
+console.log(output);
