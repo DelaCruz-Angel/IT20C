66f6c78b8666d7bb4c295579202e89cc7d47959d
DelaCruz-Angel <mobilen1cole58@gmail.com>
2025-11-20 08:19:16 +0800

    looping

---
+++ looping.js
@@ -0,0 +1,21 @@
+// //for Loop
+// output = "";
+// for (i = 1; i<=5; i++){
+//     // console.log(i);
+//     output += i + " ";
+
+// }
+
+// console.log(output);
+
+//arrays
+output = " "
+arr = [1,2,3]
+console.log("Array length: " + arr.length);
+
+for (index = arr.length - 1; index>=0; index--){
+    // console.log(arr[i]);
+     output += arr[index] + " ";
+}
+
+console.log(output);
