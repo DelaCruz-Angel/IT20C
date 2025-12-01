455d4740312a5642240a11587e06a3b4b99e2f68
DelaCruz-Angel <mobilen1cole58@gmail.com>
2025-12-01 08:04:00 +0800

    new queue

---
+++ queue.js
@@ -0,0 +1,42 @@
+class Queue{
+    constructor(){
+        this.data={};
+        this.frontIndex = 0;
+        this.rearIndex = -1;
+    }
+
+    enqueue(element){
+        this.rearIndex++;
+        this.data[this.rearIndex] = element;
+    }
+
+    traverse(){
+        let output = "";
+        for(let i = this.frontIndex; i <= this.rearIndex; i++){
+            output += this.data[i] + " ";
+        }
+
+        console.log(output);
+    }
+
+    is_empty(){
+        return this.frontIndex > this.rearIndex;
+    }
+
+    front(){
+        //correlation statement
+        if(this.is_empty()) return null;
+        return this.data[this.frontIndex];
+    }
+
+    size(){
+        return this.rearIndex - this.frontIndex +1;
+    }
+}
+
+let queue1 = new Queue();
+queue1.enqueue(4);
+queue1.enqueue(2);
+queue1.enqueue(3);
+queue1.traverse();
+
